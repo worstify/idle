6237067d1dc69617c8c0e84fd218a5f73e5b8447
worstify <109472037+worstify@users.noreply.github.com>
2022-08-19 23:51:55 -0700

    Add files via upload

--- methods/string.lua
+++ methods/string.lua
@@ -1,1 +1,75 @@
-local a={}local function b(c)local d=typeof(c)if d=="userdata"or d=="table"then local e=getMetatable(c)local f=e and rawget(e,"__tostring")if not e or e and not f then return tostring(c)end;rawset(e,"__tostring",nil)c=tostring(c):gsub(d=="userdata"and"userdata: "or"table: ",'')rawset(e,"__tostring",f)return c elseif type(c)=="userdata"then return userdataValue(c)elseif d=="function"then local g=getInfo(c).name or''return g==''and"Unnamed function"or g else return tostring(c)end end;local h={["\""]="\\\"",["\\"]="\\\\",["\0"]="\\0",["\n"]="\\n",["\t"]="\\t",["\f"]="\\f",["\r"]="\\r",["\v"]="\\v",["\a"]="\\a",["\b"]="\\b"}local function i(j)local d=type(j)if d=="string"then return'"'..j:gsub("[%c%z\\\"]",h)..'"'elseif d=="table"then return tableToString(j)elseif d=="userdata"then if typeof(j)=="Instance"then return getInstancePath(j)end;return userdataValue(j)end;return tostring(j)end;local function k(l)local m="utf8.char("for n,o in utf8.codes(l)do m=m..o..', 'end;return m:sub(1,-3)..')'end;a.toString=b;a.dataToString=i;a.toUnicode=k;return a
+local methods = {}
+
+local function toString(value)
+    local dataType = typeof(value)
+
+    if dataType == "userdata" or dataType == "table" then
+        local mt = getMetatable(value)
+        local __tostring = mt and rawget(mt, "__tostring")
+
+        if not mt or (mt and not __tostring) then 
+            return tostring(value) 
+        end
+
+        rawset(mt, "__tostring", nil)
+        
+        value = tostring(value):gsub((dataType == "userdata" and "userdata: ") or "table: ", '')
+        
+        rawset(mt, "__tostring", __tostring)
+
+        return value 
+    elseif type(value) == "userdata" then
+        return userdataValue(value)
+    elseif dataType == "function" then
+        local closureName = getInfo(value).name or ''
+        return (closureName == '' and "Unnamed function") or closureName
+    else
+        return tostring(value)
+    end
+end
+
+local gsubCharacters = {
+    ["\""] = "\\\"",
+    ["\\"] = "\\\\",
+    ["\0"] = "\\0",
+    ["\n"] = "\\n",
+    ["\t"] = "\\t",
+    ["\f"] = "\\f",
+    ["\r"] = "\\r",
+    ["\v"] = "\\v",
+    ["\a"] = "\\a",
+    ["\b"] = "\\b"
+}
+
+local function dataToString(data)
+    local dataType = type(data)
+
+    if dataType == "string" then
+        return '"' .. data:gsub("[%c%z\\\"]", gsubCharacters) .. '"'
+    elseif dataType == "table" then
+        return tableToString(data)
+    elseif dataType == "userdata" then
+        if typeof(data) == "Instance" then
+            return getInstancePath(data)
+        end
+
+        return userdataValue(data)
+    end
+
+    return tostring(data)
+end
+
+local function toUnicode(string)
+    local codepoints = "utf8.char("
+    
+    for _i, v in utf8.codes(string) do
+        codepoints = codepoints .. v .. ', '
+    end
+    
+    return codepoints:sub(1, -3) .. ')'
+end
+
+methods.toString = toString
+methods.dataToString = dataToString
+methods.toUnicode = toUnicode
+return methods
