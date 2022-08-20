3c7d5933c9f41aeeafa517c2bd9a30d82b081e01
worstify <109472037+worstify@users.noreply.github.com>
2022-08-19 23:47:35 -0700

    Add files via upload

--- ohaux.lua
+++ ohaux.lua
@@ -1,1 +1,54 @@
-local a={}local b=getgc;local c=debug.getinfo or getinfo;local d=debug.getupvalue or getupvalue or getupval;local e=debug.getconstants or getconstants or getconsts;local f=is_synapse_function or issentinelclosure or is_protosmasher_closure or is_sirhurt_closure or istempleclosure or checkclosure;local g=islclosure or is_l_closure or iscclosure and function(h)return not iscclosure(h)end;assert(b and c and e and f,"Your exploit is not supported")local i=newproxy(false)local function j(k,l)if not l then return true end;local m=e(k)for n,o in pairs(l)do if m[n]~=o and o~=i then return false end end;return true end;local function p(q,r,s,m)for t,u in pairs(b())do local v=rawget(getfenv(u),"script")if type(u)=="function"and g(u)and not f(u)and(q==nil and v.Parent==nil or q==v)and pcall(d,u,s)then if r and r~="Unnamed function"and c(u).name==r and j(u,m)then return u elseif(not r or r=="Unnamed function")and j(u,m)then return u end end end end;a.placeholderUserdataConstant=i;a.searchClosure=p;return a
+local aux = {}
+
+local getGc = getgc
+local getInfo = debug.getinfo or getinfo
+local getUpvalue = debug.getupvalue or getupvalue or getupval
+local getConstants = debug.getconstants or getconstants or getconsts
+local isXClosure = is_synapse_function or issentinelclosure or is_protosmasher_closure or is_sirhurt_closure or istempleclosure or checkclosure
+local isLClosure = islclosure or is_l_closure or (iscclosure and function(f) return not iscclosure(f) end)
+
+assert(getGc and getInfo and getConstants and isXClosure, "Your exploit is not supported")
+
+local placeholderUserdataConstant = newproxy(false)
+
+local function matchConstants(closure, list)
+    if not list then
+        return true
+    end
+    
+    local constants = getConstants(closure)
+    
+    for index, value in pairs(list) do
+        if constants[index] ~= value and value ~= placeholderUserdataConstant then
+            return false
+        end
+    end
+    
+    return true
+end
+
+local function searchClosure(script, name, upvalueIndex, constants)
+    for _i, v in pairs(getGc()) do
+        local parentScript = rawget(getfenv(v), "script")
+
+        if type(v) == "function" and 
+            isLClosure(v) and 
+            not isXClosure(v) and 
+            (
+                (script == nil and parentScript.Parent == nil) or script == parentScript
+            ) 
+            and pcall(getUpvalue, v, upvalueIndex)
+        then
+            if ((name and name ~= "Unnamed function") and getInfo(v).name == name) and matchConstants(v, constants) then
+                return v
+            elseif (not name or name == "Unnamed function") and matchConstants(v, constants) then
+                return v
+            end
+        end
+    end
+end
+
+aux.placeholderUserdataConstant = placeholderUserdataConstant
+aux.searchClosure = searchClosure
+
+return aux
