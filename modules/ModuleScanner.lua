63aecb8b9b574446ba2ea25d00d057288bb2e8c6
worstify <109472037+worstify@users.noreply.github.com>
2022-08-20 00:00:51 -0700

    Add files via upload

--- modules/ModuleScanner.lua
+++ modules/ModuleScanner.lua
@@ -1,1 +1,27 @@
-local a={}local b=import("objects/ModuleScript")local c={["getMenv"]=true,["getProtos"]=true,["getConstants"]=true,["getScriptClosure"]=true,["getLoadedModules"]=true}local function d(e)local f={}e=e or""for g,h in pairs(getLoadedModules())do if h.Name:lower():find(e)then f[h]=b.new(h)end end;return f end;a.Scan=d;a.RequiredMethods=c;return a
+local ModuleScanner = {}
+local ModuleScript = import("objects/ModuleScript")
+
+local requiredMethods = {
+    ["getMenv"] = true,
+    ["getProtos"] = true,
+    ["getConstants"] = true,
+    ["getScriptClosure"] = true,
+    ["getLoadedModules"] = true
+}
+
+local function scan(query)
+    local modules = {}
+    query = query or ""
+    
+    for _i, module in pairs(getLoadedModules()) do
+        if module.Name:lower():find(query) then
+            modules[module] = ModuleScript.new(module)
+        end
+    end
+
+    return modules
+end
+
+ModuleScanner.Scan = scan
+ModuleScanner.RequiredMethods = requiredMethods
+return ModuleScanner
