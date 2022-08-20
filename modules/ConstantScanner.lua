fffdd5eac61bac6586f8f6108db9fb8efcb3b681
worstify <109472037+worstify@users.noreply.github.com>
2022-08-19 23:48:28 -0700

    Add files via upload

--- modules/ConstantScanner.lua
+++ modules/ConstantScanner.lua
@@ -1,1 +1,55 @@
-local a={}local b=import("objects/Closure")local c=import("objects/Constant")local d={["getGc"]=true,["getInfo"]=true,["isXClosure"]=true,["getConstant"]=true,["setConstant"]=true,["getConstants"]=true}local function e(f,g)local h=type(g)local i=h=="string"and(f==g or g:lower():find(f:lower()))local j=h=="number"and(tonumber(f)==g or("%.2f"):format(g)==f)local k=h=="userdata"and toString(g)==f;if h=="function"then local l=getInfo(g).name or''return f==l or l:lower():find(f:lower())end;return i or j or k end;local function m(f)local n={}for o,p in pairs(getGc())do if type(p)=="function"and not isXClosure(p)and isLClosure(p)and not n[p]then for q,g in pairs(getConstants(p))do if e(f,g)then local r=n[p]if not r then local s=b.new(p)s.Constants[q]=c.new(s,q,g)n[p]=s else r.Constants[q]=c.new(r,q,g)end end end end end;return n end;a.Scan=m;a.RequiredMethods=d;return a
+local ConstantScanner = {}
+local Closure = import("objects/Closure")
+local Constant = import("objects/Constant")
+
+local requiredMethods = {
+    ["getGc"] = true,
+    ["getInfo"] = true,
+    ["isXClosure"] = true,
+    ["getConstant"] = true,
+    ["setConstant"] = true,
+    ["getConstants"] = true
+}
+
+local function compareConstant(query, constant)
+    local constantType = type(constant)
+
+    local stringCheck = constantType == "string" and (query == constant or constant:lower():find(query:lower()))
+    local numberCheck = constantType == "number" and (tonumber(query) == constant or ("%.2f"):format(constant) == query)
+    local userDataCheck = constantType == "userdata" and toString(constant) == query
+
+    if constantType == "function" then
+        local closureName = getInfo(constant).name or ''
+        return query == closureName or closureName:lower():find(query:lower())
+    end
+
+    return stringCheck or numberCheck or userDataCheck
+end 
+
+local function scan(query)
+    local constants = {}
+
+    for _i, closure in pairs(getGc()) do
+        if type(closure) == "function" and not isXClosure(closure) and isLClosure(closure) and not constants[closure] then
+            for index, constant in pairs(getConstants(closure)) do
+                if compareConstant(query, constant) then
+                    local storage = constants[closure]
+
+                    if not storage then
+                        local newClosure = Closure.new(closure)
+                        newClosure.Constants[index] = Constant.new(newClosure, index, constant)
+                        constants[closure] = newClosure
+                    else
+                        storage.Constants[index] = Constant.new(storage, index, constant)
+                    end
+                end
+            end
+        end
+    end
+
+    return constants
+end
+
+ConstantScanner.Scan = scan
+ConstantScanner.RequiredMethods = requiredMethods
+return ConstantScanner
